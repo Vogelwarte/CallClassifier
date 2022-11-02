6a938df6c6f8bcaef6e2c9af4d1e9d70b7e35ab7
PZdroik <917833+zdroyer@users.noreply.github.com>
2022-11-02 15:25:22 +0100

    Added extracting the call_type to the output.csv file

--- RunModel.py
+++ RunModel.py
@@ -1,12 +1,12 @@
 from pathlib import Path
-from typing import List
-
 import pandas as pd
 from opensoundscape.torch.models.cnn import load_model
 
 
 #Setup Data for Model
-curlew_table = pd.read_csv(Path("C:\\EcoHack\\classified_tables_audio\\OutPut\\Chunks\\20221101_120529UTC_summary.txt")).set_index('filename')
+from pandas import DataFrame
+
+curlew_table = pd.read_csv(Path(r'training_data\chunks\summary.txt')).set_index('filename')
 #print(curlew_table.to_string())
 #curlew_table.head()
 from opensoundscape.annotations import categorical_to_one_hot
@@ -16,12 +16,12 @@ labels = curlew_table.columns.values.tolist()
 print(str(labels))
 from sklearn.model_selection import train_test_split
 train_df, validation_df = train_test_split(curlew_table, test_size=0.2, random_state=1)
-print(train_df.head())
-print(validation_df.head())
+#print(train_df.head())
+#print(validation_df.head())
 
 
 #Run Model
-model = load_model('curlew_model_50_ecpochs\\binary_train\\best.model')
+model = load_model('binary_train/best.model')
 #%%
 #create a copy of the training dataset, sampling 0 of the training samples from it
 #prediction_dataset = model.train_dataset.sample(n=0)
@@ -41,9 +41,16 @@ model = load_model('curlew_model_50_ecpochs\\binary_train\\best.model')
 #
 # In[19]:
 #valid_scores_df, valid_preds_df, valid_labels_df = model.predict(prediction_dataset, binary_preds='multi_target', activation_layer='sigmoid')
-validation_df=validation_df.head()
+
+validation_df = validation_df.head()
 valid_scores_df, valid_preds_df, valid_labels_df = model.predict(validation_df, binary_preds='single_target', activation_layer='softmax')
 
-valid_scores_df.assign(call_type=valid_scores_df.index[-6:-5])
+report_file: Path = "output.csv"
+with open(report_file, "w") as rf:
+    results_df:DataFrame = valid_scores_df
+    print(f" filename; begin; end; call_type ; type1  ;type2  ; type3  ; type4", file=rf)
+    for row in results_df.itertuples():
+        print(f" \"{(row[0][0])}\"; {row[0][1]}s ; {row[0][2]}s ; type{(row[0][0])[-6:-5]} ; {row[1]:.2f}  ; {row[2]:.2f}  ; {row[3]:.2f}  ; {row[4]:.2f} ", file=rf)
 
-print(valid_scores_df.to_string())
+#print(valid_scores_df.to_string())
+#print (results_df.to_string(
