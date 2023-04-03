e5dd2839944e4a89f1d752f5d7d84f15089d9e88
PZdroik <917833+zdroyer@users.noreply.github.com>
2023-04-03 12:07:26 +0200

    Corrected output to utc time, added logging per model and file, reformatted whitespaces (PyCharm)

--- classify_calls_RockPtarmigan.py
+++ classify_calls_RockPtarmigan.py
@@ -6,6 +6,7 @@ import subprocess
 import sys
 import time
 from datetime import datetime
+from math import ceil
 
 import pandas as pd
 
@@ -18,6 +19,7 @@ from opensoundscape.torch.models.cnn import load_model, CNN
 from opensoundscape.audio import Audio
 
 from BNResultTools.files_dirs_tools import list_of_files_ex
+from training_chunks_from_Raven import to_hh_mm_ss
 
 usage = """
     Classifies the Rock Ptarmigan calls from the audio file
@@ -32,14 +34,15 @@ usage = """
 
 
 class SingleInputDefinition:
-    def __init__(self, in_files: List[Path], base_input_dir: Path,  output_dir: Path):
+    def __init__(self, in_files: List[Path], base_input_dir: Path, output_dir: Path):
         self.input_files: List[Path] = in_files
         self.base_input_dir = base_input_dir
         self.output_dir: Path = output_dir
 
 
 def parse_command_line() -> SingleInputDefinition:
-    parser = argparse.ArgumentParser(description='Classify Eurasian Curlew calls from audio file(s). Supported formats: wav')
+    parser = argparse.ArgumentParser(
+        description='Classify Eurasian Curlew calls from audio file(s). Supported formats: wav')
     parser.add_argument('--i',
                         help='Path to input file or folder. If this is a file, --o needs to be a file too.')
     parser.add_argument('--o',
@@ -51,7 +54,7 @@ def parse_command_line() -> SingleInputDefinition:
         in_p = Path(args.i)
         base_dir: Path = in_p.parent
         if in_p.is_dir():
-            if_list = list_of_files_ex(in_p, extensions )
+            if_list = list_of_files_ex(in_p, extensions)
             base_dir = in_p
         else:
             if in_p.is_file():
@@ -70,7 +73,7 @@ def parse_command_line() -> SingleInputDefinition:
         else:
             os.makedirs(out_dir, exist_ok=True)
 
-        dt: datetime = datetime.now()
+        dt: datetime = datetime.utcnow()
         suffix: str = dt.strftime("%Y%m%d_%H%M%SUTC")
         out_dir = out_dir / f'classification_RockPtarmigan_{suffix}'
         os.makedirs(out_dir, exist_ok=True)
@@ -98,7 +101,7 @@ def get_birdnet_results(infile: Path, output_dir: Path, common_name: str, confid
          "--rtype", "audacity",
          "--slist", str(Path("..") / "binary_train")
          ])
-    #print(f'\nCompleteProcess: {cp}')
+    # print(f'\nCompleteProcess: {cp}')
     if cp.returncode != 0:
         raise RuntimeError(f'BirdNET analysis failed: {cp}')
     bn_out_df: DataFrame = pd.read_table(bnOutFP, header=None, names=["start_time", "end_time", "name", "confidence"])
@@ -111,7 +114,7 @@ def get_birdnet_results(infile: Path, output_dir: Path, common_name: str, confid
     selected_df2["name"] = common_name
     selected_df2.reset_index(drop=True, inplace=True)
     print(f'BirdNET found {len(selected_df2)} {common_name} calls')
-    #print(selected_df2)
+    # print(selected_df2)
 
     return selected_df2
 
@@ -125,6 +128,7 @@ def type_of_maxval(row) -> Series:
 
     return pd.Series([f'type{ct}', vect4[ct - 1]])
 
+
 def load_cnn_models(sample_rate: int) -> {str, CNN}:
     freeze_support()
     result_dict: {str, CNN} = {}
@@ -133,12 +137,12 @@ def load_cnn_models(sample_rate: int) -> {str, CNN}:
                    "inception_v3_24kHz_3.0s"]
     # "resnet101_24kHz_3.0s",
     # "resnet152_24kHz_3.0s",
-
     for mn in model_names:
         model: CNN = load_model(f'bird_data/Rock_ptarmigan/models/{mn}/best.model')
         model.preprocessor.pipeline.load_audio.set(sample_rate=sample_rate)
         result_dict[mn] = model
     return result_dict
+
 
 def get_number_of_cpus() -> int:
     try:
@@ -147,7 +151,8 @@ def get_number_of_cpus() -> int:
         pass
     return os.cpu_count()
 
-def run_call_classifier_model(audio_file: Path, model:CNN) -> DataFrame: #, curlew_calls_df: DataFrame) -> DataFrame:
+
+def run_call_classifier_model(audio_file: Path, model: CNN) -> DataFrame:  # , curlew_calls_df: DataFrame) -> DataFrame:
     # print("\nClassifying the call types ...\n")
     n_cpus = get_number_of_cpus()
     n_workers: int = max(1, n_cpus - 4)
@@ -155,27 +160,27 @@ def run_call_classifier_model(audio_file: Path, model:CNN) -> DataFrame: #, curl
     scores_df = model.predict(num_workers=n_workers,
                               samples=[str(audio_file)],
                               split_files_into_clips=True,
-                              overlap_fraction=2.0/3.0,
+                              overlap_fraction=2.0 / 3.0,
                               final_clip='full',
                               activation_layer='sigmoid')
     results_df: DataFrame = scores_df
     results_df.reset_index()
 
-    #print("Raw model results: \n")
-    #print(results_df)
+    # print("Raw model results: \n")
+    # print(results_df)
 
     return results_df
 
 
 def write_BirdNET_like_result(out_fp: Path, data: DataFrame):
     idx_name = "Selection"
-    headers: List[str] = [ "View", "Channel", "Begin Time(s)", "End Time(s)", "Low Freq(Hz)",
+    headers: List[str] = ["View", "Channel", "Begin Time(s)", "End Time(s)", "Low Freq(Hz)",
                           "High Freq(Hz)", "Species Code", "Common Name", "Confidence"]
-                        #exemplary row (* - to be replaced with values)
-                        # *	Spectrogram 1	1	1954.0*	1957.0*	150
-                        # 12000	rocpta1	Rock Ptarmigan	0.2588*
+    # exemplary row (* - to be replaced with values)
+    # *	Spectrogram 1	1	1954.0*	1957.0*	150
+    # 12000	rocpta1	Rock Ptarmigan	0.2588*
     bn_result: DataFrame = data.apply(
-        lambda  row: [
+        lambda row: [
             'Spectrogram 1', 1, row.start_time, row.end_time, 150,
             12000, 'rocpta1', 'Rock Ptarmigan', row.RockPtarmigan],
         axis=1,
@@ -185,78 +190,102 @@ def write_BirdNET_like_result(out_fp: Path, data: DataFrame):
     else:
         bn_result.columns = headers
     bn_result.index.rename(name=idx_name, inplace=True)
-    bn_result.index += 1 #start the records with 1 (not 0)
+    bn_result.index += 1  # start the records with 1 (not 0)
 
-    #print(bn_result.head())
-    bn_result.to_csv(path_or_buf= out_fp, header=True, index=True, sep='\t', float_format="%.2f")
+    # print(bn_result.head())
+    bn_result.to_csv(path_or_buf=out_fp, header=True, index=True, sep='\t', float_format="%.2f")
+
 
 def write_Audacity_labels_result(out_fp: Path, data: DataFrame):
-
     report_columns = ["start_time", "end_time", "annotation"]  #
-    #input columns: [start_time_, end_time, _noise, RockPtarmigan]
+    # input columns: [start_time_, end_time, _noise, RockPtarmigan]
 
     audacity_labels: DataFrame = data.apply(
         lambda row: [row.start_time, row.end_time, f'RockPtarmigan:{row.RockPtarmigan:.2f}_NOT:{row._noise:.2f}'],
         axis=1,
         result_type='expand')
 
-    #print(audacity_labels.head())
-    audacity_labels.to_csv(path_or_buf = out_fp, header=False, index=False, sep='\t', float_format="%.2f")
+    # print(audacity_labels.head())
+    audacity_labels.to_csv(path_or_buf=out_fp, header=False, index=False, sep='\t', float_format="%.2f")
+
 
 def classify_audio_file(in_audio_fp: Path, output_dir: Path, model: CNN, model_name: str):
     print(f'Processing file: {in_audio_fp}...')
 
     classification_result: DataFrame = run_call_classifier_model(in_audio_fp, model)
-    #expected structure: MultiIndex:[file, start_time, end_time], columns: [ _nothing, RockPtarmigan]
+    # expected structure: MultiIndex:[file, start_time, end_time], columns: [ _nothing, RockPtarmigan]
 
-    #move the indices (file, start_time, end_time) to the columns
+    # move the indices (file, start_time, end_time) to the columns
     classification_result.reset_index(inplace=True)
 
-    #strip column names from leading and trailing whitespaces
+    # strip column names from leading and trailing whitespaces
     classification_result.rename(str.strip, inplace=True, axis='columns')
 
-    classes: List[str] =  ["_noise", "RockPtarmigan"]
-    #remove all the records with score for Rock Ptarmigan below 1%
-    classification_result.drop(classification_result[classification_result[classes[1]]<0.01].index, inplace=True)
+    classes: List[str] = ["_noise", "RockPtarmigan"]
+    # remove all the records with score for Rock Ptarmigan below 1%
+    classification_result.drop(classification_result[classification_result[classes[1]] < 0.01].index, inplace=True)
 
-    #drop the file columne (anyway its about one audio file {in_audio_fp} only)
+    # drop the file columne (anyway its about one audio file {in_audio_fp} only)
     classification_result.drop(columns=['file'], inplace=True, axis=1)
 
     file_name_no_ext: str = in_audio_fp.stem
 
-    #classification_result DataFrame format: (numeric index), [start_time_, end_time, _noise, RockPtarmigan]
+    # classification_result DataFrame format: (numeric index), [start_time_, end_time, _noise, RockPtarmigan]
     cc_result_fp: Path = output_dir / f'{file_name_no_ext}.RpCC.selection.table.txt'
     write_BirdNET_like_result(cc_result_fp, classification_result)
 
     audacity_labels_fp: Path = output_dir / (f'labels-{model_name}_{file_name_no_ext}.txt')
-    write_Audacity_labels_result(audacity_labels_fp, classification_result )
+    write_Audacity_labels_result(audacity_labels_fp, classification_result)
 
     print(f'End of processing, output file: {in_audio_fp}')
+
 
 def do_the_stuff():
     input_def: SingleInputDefinition = parse_command_line()
     sample_rate: int = 24000  # Hz
     chunk_length: int = 3  # seconds;
-    min_audio_size: float = 0.3 * 2 * sample_rate * chunk_length    # 16bit = 2Bytes, 0.3 - optimistic flac compression rate
+    min_audio_size: float = 0.3 * 2 * sample_rate * chunk_length  # 16bit = 2Bytes, 0.3 - optimistic flac compression rate
     models = load_cnn_models(sample_rate)
     for mn in models.keys():
-        with open( input_def.output_dir / f'{mn}_log.txt','w') as log_file:
+        mstart_proc: float = time.time()
+        files_done: int = 0
+        files_omitted: int = 0
+        errors: int = 0
+        with open(input_def.output_dir / f'{mn}_log.txt', 'w') as log_file:
+            print(f'Classification by model [{mn}], '
+                  f'start time: {datetime.utcnow().strftime("%Y%m%d_%H%M%SUTC")}',
+                  file=log_file, flush=True)
             for in_fp in input_def.input_files:
+                fstart_time: float = time.time()
                 try:
-                    if in_fp.stat().st_size > min_audio_size:
-                        file_out_dir: Path = input_def.output_dir / Path(mn) / in_fp.relative_to(input_def.base_input_dir).parent
+                    file_size: int = in_fp.stat().st_size
+                    if file_size > min_audio_size:
+                        file_out_dir: Path = input_def.output_dir / Path(mn) / in_fp.relative_to(
+                            input_def.base_input_dir).parent
                         os.makedirs(file_out_dir, exist_ok=True)
                         classify_audio_file(in_fp, file_out_dir, models[mn], mn)
+                        files_done += 1
+                        print(f'Processing time {time.time() - fstart_time:.1f}s, '
+                              f'file size {file_size / 1024.0 / 1024.0:.1f}MB, '
+                              f'file:[{in_fp}]', file=log_file, flush=True)
                     else:
-                        raise RuntimeWarning(f'File size too small: '
-                                             f'{in_fp.stat().st_size/1024.0:.1f}kB, '
-                                             f'minimun: {min_audio_size/1024.0:.1f}kB,'
-                                             f'path: {in_fp}')
+                        files_omitted += 1
+                        print(f'File size too small: '
+                              f'{in_fp.stat().st_size / 1024.0:.1f}kB, '
+                              f'min: {min_audio_size / 1024.0:.1f}kB, '
+                              f'file:[{in_fp}]', file=log_file)
+                    log_file.flush()
                 except Exception as ex:
+                    errors += 1
                     print(f'Exception with model {mn}, audio file {in_fp}:\n{ex}', file=log_file)
                 except:
-                    print(f'Unknown error with model {mn}, audio file {in_fp}', file=sys.log_file)
-
+                    errors += 1
+                    print(f'Unknown error with model {mn}, audio file {in_fp}', file=log_file)
+            mstart_proc = time.time() - mstart_proc
+            print(f'Model {mn}, files done: {files_done}, omitted: {files_omitted}, errors:{errors}, '
+                  f'processing time: {to_hh_mm_ss(mstart_proc)} ({mstart_proc:.1f}s), '
+                  f'speed: {mstart_proc / max(1.0, files_done):.1f} s/file',
+                  file=log_file)
 
 
 @contextmanager
@@ -267,6 +296,7 @@ def set_posix_windows():
         yield
     finally:
         pathlib.PosixPath = posix_backup
+
 
 if __name__ == '__main__':
     start: float = time.time()
